7a69a03c3381621d3ac35f5d07279be675c4d353
Rodrick Bloomfield <rodrikbloomfield@yahoo.com>
2018-04-01 00:14:05 -0400

    Add styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 
-gulp.task('default', ['browser-sync'], function() {
-    gulp.watch('css/**/*.css').on('change', browserSync.reload);
+gulp.task('default', ['styles', 'browser-sync'], function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
     gulp.watch('*.html').on('change', browserSync.reload);
     gulp.watch('js/**/*.js').on('change', browserSync.reload);
+});
+
+
+gulp.task('styles', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
 });
 
 
